d86442b3b2331c973870ea9effa823146de29d1f
SilentChaos512 <sworkman256@gmail.com>
2019-07-23 20:48:03 -0500

    Update to 1.14.4

--- src/main/java/net/silentchaos512/torchbandolier/init/ModLoot.java
+++ src/main/java/net/silentchaos512/torchbandolier/init/ModLoot.java
@@ -26,7 +26,7 @@ public final class ModLoot {
         if (ADD_BANDOLIER_TO.contains(event.getName()) && !hasLootPool(event.getTable(), "torch_bandolier")) {
             event.getTable().addPool((new LootPool.Builder())
                     .name("torch_bandolier")
-                    .addEntry(ItemLootEntry.func_216168_a(ModItems.emptyTorchBandolier))
+                    .addEntry(ItemLootEntry.builder(ModItems.emptyTorchBandolier))
                     .acceptCondition(RandomChance.builder(0.2f))
                     .build());
         }
